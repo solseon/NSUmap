b99a06de552b1fbd7de86fd9a5988d235936539a
안수범 <solseon0130@gmail.com>
2024-10-03 03:40:26 +0900

    update. buildingList 파일로 클래스 옮김

--- buildingList.py
+++ buildingList.py
@@ -1,3 +1,5 @@
+import folium
+
 class building: # 건물 이름, 위치, 마킹 클래스
     def __init__(self, name, location): 
         self.name = name    # 이름변수
@@ -7,11 +9,3 @@ class building: # 건물 이름, 위치, 마킹 클래스
         popup=self.name,
         icon=folium.Icon(color=color,icon=icon)
         )
-
-building("본관", [36.9072014, 127.1430552]) #본관
-building("상경학관", [36.90818157832219, 127.14407494870781]) #상경학관
-
-markinglist = [
-    mainbuilding.marking("blue", "circle"),
-    sanggyung.marking("red", "circle")
-]
