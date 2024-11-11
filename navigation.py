56d89cdae4431b8ddff7cac5156284f563d2bef5
DaeBokkk <1semi4304@naver.com>
2024-11-11 22:28:48 +0900

    add.흡연장, 주차장 정보 저장

---
+++ navigation.py
@@ -0,0 +1,99 @@
+import folium   # folium을 호출
+
+class Building: # 건물 이름, 위치, 마킹 클래스
+    def __init__(self, name, location): 
+        self.name = name    # 이름변수
+        self.location = location    # 위치변수[위도, 경도]
+        
+    def marking(self, color, icon):   
+        return folium.Marker(
+            location=self.location,
+            popup=self.name,
+            icon=folium.Icon(color=color, icon=icon)
+        )
+        
+# 남서울대학교 중심 좌표
+center = [36.908, 127.143619452225] 
+m = folium.Map(location=center, zoom_start=16)
+
+# 건물 정보 저장
+# 본관하고 공학1,2관 분리 필요해보임
+main = Building("본관", [36.90725, 127.1431])
+engineering1 = Building("공학1관", [36.9073, 127.1436])
+engineering2 = Building("공학2관", [36.9072, 127.1426])
+sanggyeong = Building("상경학관", [36.90818157832219, 127.14407494870781])
+
+# 도로 좌표
+main_f = Building("본관 앞", [36.90755, 127.14305])
+sanggyeong_f = Building('상경학관 앞',[36.9081 ,127.14391])
+main_sanggyeong = Building('본관-상경 삼거리', [36.9077,127.1440])
+
+# 흡연장 정보 저장
+smoke_engineering1 = Building("공학1관 흡연장", [36.9072,127.1441])
+smoke_engineering2 = Building("공학2관 흡연장", [36.9073, 127.1417])
+smokek_sanggyeong = Building("상경학관 흡연장",[36.9082, 127.1443])
+smoke_healthcare = Building("보건의료학관 흡연장", [36.9089, 127.1457])
+smoke_library = Building("성암기념중앙도서관 흡연장",[36.9088, 127.1435])
+smoke_knowledgeinformation = Building("지식정보관 흡연장", [36.9098, 127.1452])
+smoke_elem1 = Building("엘림1생활관 흡연장", [36.9093, 127.1476])
+smoke_gym = Building("성암문화체육관 흡연장", [36.9100, 127.1474])
+smoke_tennis = Building("테니스장 흡연장", [36.9109, 127.1436])
+smoke_parking1 = Building("제1주차장 흡연장", [36.9099, 127.1420])
+smoke_busstop = Building("버스정류장 흡연장",[36.9107, 127.1420])
+
+
+# 주차장 정보 저장
+parking_bus = Building("버스 주차장", [36.9103, 127.1424])
+parking_parking1 = Building("제1주차장", [36.9092, 127.1416])
+parking_parking2 = Building("제2주차장", [36.9085, 127.1411])
+parking_library = Building("성앙기념중앙도서관 주차장", [36.9090, 127.1440])
+parking_studentwelfare = Building("학생복지회관 주차장", [36.9097, 127.1431])
+parking_group = Building("동아리방 주차장", [36.9110, 127.1446])
+parking_gym = Building("성함문화체육관 주차장", [36.9107, 127.1463])
+parking_elem1 = Building("엘림1생활관 주차장", [36.9091, 127.1472])
+
+
+# 건물 마킹 리스트 생성
+markinglist = list()
+
+# 건물 마킹
+markinglist.append(main.marking("red", "circle"))
+markinglist.append(engineering1.marking("red", "circle"))
+markinglist.append(engineering2.marking("red", "circle"))
+markinglist.append(sanggyeong.marking("red", "circle"))
+
+markinglist.append(smoke_engineering1.marking("green","circle"))
+markinglist.append(smoke_engineering2.marking("green","circle"))
+markinglist.append(smokek_sanggyeong.marking("green","circle"))
+markinglist.append(smoke_healthcare.marking("green","circle"))
+markinglist.append(smoke_library.marking("green","circle"))
+markinglist.append(smoke_elem1.marking("green","circle"))
+markinglist.append(smoke_gym.marking("green","circle"))
+markinglist.append(smoke_knowledgeinformation.marking("green","circle"))
+markinglist.append(smoke_tennis.marking("green","circle"))
+markinglist.append(smoke_parking1.marking("green","circle"))
+markinglist.append(smoke_busstop.marking("green","circle"))
+
+markinglist.append(parking_bus.marking("blue", "circle"))
+markinglist.append(parking_parking1.marking("blue", "circle"))
+markinglist.append(parking_parking2.marking("blue", "circle"))
+markinglist.append(parking_library.marking("blue", "circle"))
+markinglist.append(parking_studentwelfare.marking("blue", "circle"))
+markinglist.append(parking_group.marking("blue", "circle"))
+markinglist.append(parking_gym.marking("blue", "circle"))
+markinglist.append(parking_elem1.marking("blue", "circle"))
+
+# 마킹 추가
+for mark in markinglist:
+    mark.add_to(m)
+
+# 본관에서 상경학관까지의 경로를 직선으로 그리기
+folium.PolyLine(
+    locations=[main.location,main_f.location,main_sanggyeong.location, sanggyeong_f.location, sanggyeong.location],  # 경로에 있는 좌표 리스트
+    color="green",  # 선의 색깔
+    weight=5,  # 선의 두께
+    opacity=0.8  # 선의 투명도
+).add_to(m)
+m.add_child(folium.LatLngPopup())
+# 지도 출력
+m
