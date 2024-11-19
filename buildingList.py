e355146d0b3d9b7bb5c0b65e8ac2dec4862191a3
안수범 <solseon0130@gmail.com>
2024-11-19 14:36:02 +0900

    Update. html 지도 웹 구현 및 모듈화 작업

--- buildingList.py
+++ buildingList.py
@@ -9,3 +9,8 @@ class building: # 건물 이름, 위치, 마킹 클래스
         popup=self.name,
         icon=folium.Icon(color=color,icon=icon)
         )
+    def markingList(self, data):    # 객체들을 마킹해주는 함수
+        hakkwan = List()    # 객체를 담아야하는 리스트 생성
+        for i in data:      # hakkwan 리스트에 객체 데이터들을 넣어주는 작업
+            hakkwan.append(i)
+        return hakkwan # 객체가 담겨있는 리스트 반환
